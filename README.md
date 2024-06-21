a2d0defbecdd039c03827ee83cc0c08f2367e1da
crlnravel <carleanoravel@gmail.com>
2024-06-22 02:13:56 +0700

    Add readme

---
+++ README.md
@@ -0,0 +1,8 @@
+Cara jalanin:
+
+1. Install node di https://nodejs.org/en
+2. Atur file yang ingin dijalankan di index.js
+3. Jalanin perintah:
+   * `npm install`
+   * `npm run start`
+4. Hasilnya akan muncul di file data-pertanyaan.csv
--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import { scrape } from "./scraper.js";
 
 fs.writeFileSync("data-pertanyaan.csv", "")
 
-await scrape(1, 10);
+// UBAH FUNGSI INI TERGANTUNG PAGE YANG INGIN DI SCRAPE
+// CONTOH DIBAWAH UNTUK PAGE 1 - 10
+await scrape(1, 10);  // GANTI HALAMAN
 
